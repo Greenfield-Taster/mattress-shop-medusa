c83c6f5af81827d3db750d0754f8d1c423d44dd2
Anastasiia Horbachova <77613323+Greenfield-Taster@users.noreply.github.com>
2026-02-17 19:10:53 +0200

    Remove dead code: debug scripts, unused components, placeholder routes, and boilerplate READMEs

--- src/modules/customer/test-methods.ts
+++
@@ -1,6 +0,0 @@
-import CustomerModuleService from "./service"
-
-// Виводимо всі методи сервісу
-const service = CustomerModuleService.prototype
-console.log("Available methods:")
-console.log(Object.getOwnPropertyNames(service))
